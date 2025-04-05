e37d6ecfb48522c8b205ceaa58fce5d129906b5f
nizam-321 <bhainizam303@gmail.com>
2025-04-06 00:11:24 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let clock = document.getElementById("clock");
+function updateClock() {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let period = "AM";
+    if (hours >= 12){
+        period = "PM";
+        if (hours > 12){
+            hours = hours - 12;
+        }
+    }else if (hours === 0){
+        hours = 12;
+    }
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+   
+    let currentTime = hours + ":" + minutes + ":" + seconds;
+    clock.innerText = currentTime;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
